b7eb640bed8e42602f0af8d76e87c1b4e2344cc9
Soran <soran.gdr.cs@gmail.com>
2019-11-07 21:30:41 +0330

    getAdj2 function: 1. Rename inListt to input_list

--- helperFunctions/getAdjacents.py
+++ helperFunctions/getAdjacents.py
@@ -1,8 +1,8 @@
-def getAdj2(graph, inListt, n):
+def getAdj2(graph, input_list, n):
     """
 
     :param graph: NetworkX graph object
-    :param inListt: Input list
+    :param input_list: Input list
     :param n: Hop N
     :return: N hop neighbours of the input list's items
     """
@@ -10,7 +10,7 @@ def getAdj2(graph, inListt, n):
     while (n > 0):
         n -= 1
         outListt = []
-        for nd in graph.nbunch_iter(inListt):
+        for nd in graph.nbunch_iter(input_list):
             for neigh in graph[nd]:
                 if neigh not in fullListt:
                     fullListt.append(neigh)
