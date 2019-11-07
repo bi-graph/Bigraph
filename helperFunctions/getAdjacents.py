5497dd0318055fc5f76c17fab580e3749fd572d5
Soran <soran.gdr.cs@gmail.com>
2019-11-07 21:32:05 +0330

    getAdj2 function: 1. Rename fullListt to full_list

--- helperFunctions/getAdjacents.py
+++ helperFunctions/getAdjacents.py
@@ -6,13 +6,13 @@ def getAdj2(graph, input_list, n):
     :param n: Hop N
     :return: N hop neighbours of the input list's items
     """
-    fullListt = []
+    full_list = []
     while (n > 0):
         n -= 1
         outListt = []
         for nd in graph.nbunch_iter(input_list):
             for neigh in graph[nd]:
-                if neigh not in fullListt:
-                    fullListt.append(neigh)
+                if neigh not in full_list:
+                    full_list.append(neigh)
                     outListt.append(neigh)
     return outListt
