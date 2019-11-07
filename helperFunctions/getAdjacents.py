f6205346dd675d82552151309cc310de1a466c4c
Soran <soran.gdr.cs@gmail.com>
2019-11-07 21:32:32 +0330

    getAdj2 function: 1. Rename nd to node

--- helperFunctions/getAdjacents.py
+++ helperFunctions/getAdjacents.py
@@ -10,8 +10,8 @@ def getAdj2(graph, input_list, n):
     while (n > 0):
         n -= 1
         outListt = []
-        for nd in graph.nbunch_iter(input_list):
-            for neigh in graph[nd]:
+        for node in graph.nbunch_iter(input_list):
+            for neigh in graph[node]:
                 if neigh not in full_list:
                     full_list.append(neigh)
                     outListt.append(neigh)
