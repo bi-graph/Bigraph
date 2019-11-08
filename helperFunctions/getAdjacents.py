6b33fdf63db650b700b4d9d111fec22ae3e9170b
Soran <soran.gdr.cs@gmail.com>
2019-11-08 21:36:00 +0330

    getAdj2 function: 1. Rename n to hop_n

--- helperFunctions/getAdjacents.py
+++ helperFunctions/getAdjacents.py
@@ -1,14 +1,14 @@
-def getAdj2(graph, input_list, n):
+def getAdj2(graph, input_list, hop_n):
     """
-    Find hop-N neighbours of each item in input_list
+    Find hop-hop_n neighbours of each item in input_list
     :param graph: NetworkX graph object
     :param input_list: Input list
-    :param n: Hop N
-    :return: N hop neighbours of the input list's items
+    :param hop_n: Hop hop_n
+    :return: hop_n hop neighbours of the input list's items
     """
     full_list = []
-    while (n > 0):
-        n -= 1
+    while (hop_n > 0):
+        hop_n -= 1
         output_list = []
         for node in graph.nbunch_iter(input_list):
             for neighbour in graph[node]:
