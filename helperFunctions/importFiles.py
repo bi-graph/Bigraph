3832584d8dabb9afdaf1d6e43bdf43c7c6753ed0
Soran <soran.gdr.cs@gmail.com>
2019-11-08 21:42:23 +0330

    import_files function: 1. Implement

--- helperFunctions/importFiles.py
+++ helperFunctions/importFiles.py
@@ -4,3 +4,6 @@ def import_files():
 
     :return:
     """
+    df = pd.read_csv('./inputs/SNP-Cancer Dataset.csv', sep=',')
+    df_nodes = pd.read_csv('./inputs/id_labels.csv', sep=',')
+    # df = df.drop(['Weight'], axis=1)
