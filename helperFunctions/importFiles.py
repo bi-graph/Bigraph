e3a4d7890efb73f4ce79b9aac6651852ff1215f5
Soran <soran.gdr.cs@gmail.com>
2019-11-08 21:41:08 +0330

    importFiles.py module: 1. Initiate import_files function

--- helperFunctions/importFiles.py
+++ helperFunctions/importFiles.py
@@ -1,13 +1,6 @@
-import pandas as pd
-
 
 def import_files():
-    print('Importing files and making dataframes...')
-    df = pd.read_csv('./inputs/SNP-Cancer Dataset.csv', sep=',')
-    df_nodes = pd.read_csv('./inputs/id_labels.csv', sep=',')
-    # df = df.drop(['Weight'], axis=1)
+    """
 
-    dic = {}
-    for row in df_nodes.iterrows():
-        dic.update({row[1][0]: row[1][1]})
-    return df, dic
+    :return:
+    """
