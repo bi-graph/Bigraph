cb75fe8c9fa622b00ecfe60c231582d67cf8a946
Soran <soran.gdr.cs@gmail.com>
2019-11-08 21:45:10 +0330

    import_files function: 1. Refactor input csv file

--- helperFunctions/importFiles.py
+++ helperFunctions/importFiles.py
@@ -5,7 +5,7 @@ def import_files():
 
     :return:
     """
-    df = pd.read_csv('./inputs/SNP-Cancer Dataset.csv', sep=',')
+    df = pd.read_csv('./inputs/neighbour_matrix.csv', sep=',')
     df_nodes = pd.read_csv('./inputs/id_labels.csv', sep=',')
     # df = df.drop(['Weight'], axis=1)
 
