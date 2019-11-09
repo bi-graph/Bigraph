ad9e4eb0c909f0a673edcface912875a933acfe9
Soran <soran.gdr.cs@gmail.com>
2019-11-09 22:13:17 +0330

    import_files function: 1. Rename df_nodes to raw_label_dictionary

--- helperFunctions/importFiles.py
+++ helperFunctions/importFiles.py
@@ -7,10 +7,10 @@ def import_files():
     """
     print('Importing files and making dataframes...')
     raw_adjacency_dictionary = pd.read_csv('./inputs/neighbour_matrix.csv', sep=',')
-    df_nodes = pd.read_csv('./inputs/id_labels.csv', sep=',')
+    raw_label_dictionary = pd.read_csv('./inputs/id_labels.csv', sep=',')
     # raw_adjacency_dictionary = raw_adjacency_dictionary.drop(['Weight'], axis=1)
 
     label_dictionary = {}
-    for row in df_nodes.iterrows():
+    for row in raw_label_dictionary.iterrows():
         label_dictionary.update({row[1][0]: row[1][1]})
     return raw_adjacency_dictionary, label_dictionary
