7b9842642c52bf0eeb9a4099ff61d62c5500f2b3
Soran <soran.gdr.cs@gmail.com>
2019-11-09 22:00:42 +0330

    import_files function: 1. Print status of running the function

--- helperFunctions/importFiles.py
+++ helperFunctions/importFiles.py
@@ -5,6 +5,7 @@ def import_files():
 
     :return:
     """
+    print('Importing files and making dataframes...')
     df = pd.read_csv('./inputs/neighbour_matrix.csv', sep=',')
     df_nodes = pd.read_csv('./inputs/id_labels.csv', sep=',')
     # df = df.drop(['Weight'], axis=1)
