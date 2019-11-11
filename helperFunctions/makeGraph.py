f5afa6b918e31471968ae813cc4f2472425d6434
Soran <soran.gdr.cs@gmail.com>
2019-11-12 00:30:37 +0330

    makeGraph.py module: 1. Implement

--- helperFunctions/makeGraph.py
+++ helperFunctions/makeGraph.py
@@ -11,3 +11,10 @@ def make_graph(df):
     G = nx.Graph()
     G.add_nodes_from(df['SNP'], bipartite=0)
     G.add_nodes_from(df['Cancer'], bipartite=1)
+    for i in df.iterrows():
+        nodeList.append(i[1][0])
+        nodeList.append(i[1][1])
+
+    G.add_edges_from(add_to_list(df))
+    isolates = nx.isolates(G)
+    # G.remove_nodes_from(isolates)
