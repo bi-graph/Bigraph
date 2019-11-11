8fa2f12daac1a72d093154f74a5f0c8239dd268e
Soran <soran.gdr.cs@gmail.com>
2019-11-12 00:29:48 +0330

    makeGraph.py module: 1. Add node list 2. Make networkx graph

--- helperFunctions/makeGraph.py
+++ helperFunctions/makeGraph.py
@@ -4,3 +4,7 @@ def make_graph(df):
     :param df:
     :return:
     """
+    nodeList = []
+    G = nx.Graph()
+    G.add_nodes_from(df['SNP'], bipartite=0)
+    G.add_nodes_from(df['Cancer'], bipartite=1)
