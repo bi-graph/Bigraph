059ac40a4d26110d08edc920c449c4fae6bb7a5d
Soran <soran.gdr.cs@gmail.com>
2019-11-13 01:44:31 +0330

    add_to_list function: 1. Renate edgeLIst to edge_list

--- helperFunctions/pdToList.py
+++ helperFunctions/pdToList.py
@@ -1,12 +1,12 @@
-def add_to_list(df: dict) -> list:
+def add_to_list(dataframe: dict) -> list:
     """
     Generate link tuples and append them to a list
-    :param df: Dataframe containing links
+    :param dataframe: Dataframe containing links
     :return: Edge list
     """
-    edgeList = []
-    for i in df.iterrows():
+    edge_list = []
+    for i in dataframe.iterrows():
         # if not str(df_nodes[i[1][1]]).__contains__('.'):
         tuples = i[1][0], i[1][1]
-        edgeList.append(tuples)
-    return edgeList
+        edge_list.append(tuples)
+    return edge_list
