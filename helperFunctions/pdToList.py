007668c051320139660c68795f28a709266631a6
Soran <soran.gdr.cs@gmail.com>
2019-11-13 01:34:44 +0330

    pdToList.py module: 1. Initiate add_to_list function 2. Add docstring

--- helperFunctions/pdToList.py
+++ helperFunctions/pdToList.py
@@ -1,7 +1,6 @@
 def add_to_list(df):
-    edgeList = []
-    for i in df.iterrows():
-        # if not str(df_nodes[i[1][1]]).__contains__('.'):
-        tuples = i[1][0], i[1][1]
-        edgeList.append(tuples)
-    return edgeList
+    """
+
+    :param df:
+    :return:
+    """
