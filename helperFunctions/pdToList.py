0abe572eb423e7901843e70d3d6ee9efe2cead2f
Soran <soran.gdr.cs@gmail.com>
2019-11-14 01:47:06 +0330

    add_to_list function: 1. Renate ii to edge_row

--- helperFunctions/pdToList.py
+++ helperFunctions/pdToList.py
@@ -5,8 +5,8 @@ def add_to_list(dataframe: dict) -> list:
     :return: Edge list
     """
     edge_list = []
-    for i in dataframe.iterrows():
-        # if not str(df_nodes[i[1][1]]).__contains__('.'):
-        tuples = i[1][0], i[1][1]
+    for edge_row in dataframe.iterrows():
+        # if not str(df_nodes[edge_row[1][1]]).__contains__('.'):
+        tuples = edge_row[1][0], edge_row[1][1]
         edge_list.append(tuples)
     return edge_list
