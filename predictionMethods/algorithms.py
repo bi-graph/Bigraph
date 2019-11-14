1cbbf681b1ecff83cb448f652c72855f015f1711
Soran <soran.gdr.cs@gmail.com>
2019-11-15 02:08:18 +0330

    adamic_adar function: 1. Refactor variable names

--- predictionMethods/algorithms.py
+++ predictionMethods/algorithms.py
@@ -1,13 +1,13 @@
 
-def adamic_adar(setone, settwo, G):
+def adamic_adar(set_one, set_two, G):
     """
-
-    :param setone:
-    :param settwo:
+    
+    :param set_one:
+    :param set_two:
     :param G:
     :return:
     """
-    intersection = (set(setone) & set(settwo))
+    intersection = (set(set_one) & set(set_two))
     sum = 0
     for i in intersection:
         # Get neighbors count
