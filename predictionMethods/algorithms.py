2d624dae5117f68f214af285b383f4a3e389cff9
Soran <soran.gdr.cs@gmail.com>
2019-11-15 02:08:43 +0330

    adamic_adar function: 1. Refactor variable names 2

--- predictionMethods/algorithms.py
+++ predictionMethods/algorithms.py
@@ -9,9 +9,9 @@ def adamic_adar(set_one, set_two, G):
     """
     intersection = (set(set_one) & set(set_two))
     sum = 0
-    for i in intersection:
+    for node in intersection:
         # Get neighbors count
-        deg = set(G[i]).__len__()
+        deg = set(G[node]).__len__()
         if (deg > 1):
             sum += (math.log(deg)) ** -1
         else:
