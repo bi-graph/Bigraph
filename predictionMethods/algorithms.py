c16feba28a07f08cfbf2ca7ca46e14a79ff3e8b2
Soran <soran.gdr.cs@gmail.com>
2019-11-15 02:06:51 +0330

    adamic_adar function: 1. Implement

--- predictionMethods/algorithms.py
+++ predictionMethods/algorithms.py
@@ -7,3 +7,12 @@ def adamic_adar(setone, settwo, G):
     :param G:
     :return:
     """
+    intersection = (set(setone) & set(settwo))
+    sum = 0
+    for i in intersection:
+        # Get neighbors count
+        deg = set(G[i]).__len__()
+        if (deg > 1):
+            sum += (math.log(deg)) ** -1
+        else:
+            sum += 0
