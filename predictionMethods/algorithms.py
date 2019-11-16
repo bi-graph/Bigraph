c0829e45fbb057219bb5f71909cb31a9876d9f54
Soran <soran.gdr.cs@gmail.com>
2019-11-17 02:20:07 +0330

    adamic_adar function: 1. Rename G to graph

--- predictionMethods/algorithms.py
+++ predictionMethods/algorithms.py
@@ -1,18 +1,18 @@
 import math
 
-def adamic_adar(set_one: list, set_two: list, G) -> float:
+def adamic_adar(set_one: list, set_two: list, graph) -> float:
     """
     Calculate Adamic Adar score for input lists
     :param set_one: A list of graph nodes -> part one
     :param set_two: A list of graph nodes -> part two
-    :param G: NetworkX bipartite graph
+    :param graph: NetworkX bipartite graph
     :return: Adamic Adar score
     """
     intersection = (set(set_one) & set(set_two))
     sum = 0
     for node in intersection:
         # Get neighbors count
-        degree = set(G[node]).__len__()
+        degree = set(graph[node]).__len__()
         if degree > 1:
             sum += (math.log(degree)) ** -1
         else:
