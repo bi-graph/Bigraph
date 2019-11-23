44d20adf53437a8c75ab0f926102f7e68e8219f2
Soran <soran.gdr.cs@gmail.com>
2019-11-23 04:28:34 +0330

    jc_predict function: 1. Find hop 2 neighbours

--- bigraph/bigraph.py
+++ bigraph/bigraph.py
@@ -21,3 +21,17 @@ def jc_predict(G):
     out.write(",")
     out.write('Probability')
     out.write("\n")
+    exception_count = 0
+    for left_element in left_set:
+        hop2s[left_element] = getAdj2(G, list(set(G[left_element])), 1)
+        for right_element in right_set:
+            neighbors[right_element] = list(set(G[right_element]))
+            if not (left_element, right_element) in G.edges:
+                try:
+                    jaccard_sim[left_element][right_element] = jaccard(hop2s[(left_element)],
+                                                                       neighbors[(right_element)])
+                    if jaccard_sim[left_element][right_element] > 0:
+                        dictionary.update({(left_element, right_element): jaccard_sim[left_element][right_element]})
+                except:
+                    exception_count += 1
+                    print(exception_count)
