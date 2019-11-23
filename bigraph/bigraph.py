76549cb7e460a318ac6aca2334afda44d69fab43
Soran <soran.gdr.cs@gmail.com>
2019-11-23 04:25:37 +0330

    jc_predict function: 1. Import input csv files 2. Create left and right side lists 3. Necessary dictionaries and variables

--- bigraph/bigraph.py
+++ bigraph/bigraph.py
@@ -5,3 +5,14 @@ def jc_predict(G):
     :param G:
     :return:
     """
+    start_jc = datetime.now()
+
+    # print('Jaccard prediction starting...')
+    dictionary = {}
+    out = open('./predictions/jaccard.csv', 'w')
+    outN = open('./predictions/jaccard_with_name.csv', 'w')
+    hop2s = dict()
+    neighbors = dict()
+    jaccard_sim = defaultdict(dict)
+    left_set = [n for n, d in G.nodes(data=True) if d['bipartite'] == 0]
+    right_set = [n for n, d in G.nodes(data=True) if d['bipartite'] == 1]
