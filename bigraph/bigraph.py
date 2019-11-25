c138a49f7379cc84830032964ebdab9f593fd7f4
Soran <soran.gdr.cs@gmail.com>
2019-11-25 04:43:10 +0330

    jc_predict function: 1. Import the bigraph.algorithms module

--- bigraph/bigraph.py
+++ bigraph/bigraph.py
@@ -2,8 +2,7 @@ from _operator import itemgetter
 from collections import defaultdict
 from datetime import datetime
 
-# from bigraph.algorithms import jaccard
-from scipy.spatial.distance import jaccard
+from bigraph.algorithms import jaccard
 
 from helperFunctions.getAdjacents import getAdj2
 
