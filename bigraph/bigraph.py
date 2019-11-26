d68eddbef5b84158338bdace5c6b29325c1f8b2f
Soran <soran.gdr.cs@gmail.com>
2019-11-26 04:54:29 +0330

    aa_predict function: 1. Calculate score

--- bigraph/bigraph.py
+++ bigraph/bigraph.py
@@ -77,3 +77,19 @@ def aa_predict(G):
     out.write(",")
     out.write('Probability')
     out.write("\n")
+
+    exception_count = 0
+    for left_element in left_set:
+        hop2s[left_element] = getAdj2(G, list(set(G[left_element])), 1)
+        for right_element in right_set:
+            neighbors[right_element] = list(set(G[right_element]))
+            if not (left_element, right_element) in G.edges:
+                try:
+                    aa_sim[left_element][right_element] = adamic_adar(hop2s[(left_element)],
+                                                                      neighbors[(right_element)], G)
+                    if aa_sim[left_element][right_element] > 0:
+                        # print(left_element, right_element, aa_sim[left_element][right_element])
+                        dictionary.update({(left_element, right_element): aa_sim[left_element][right_element]})
+                except:
+                    exception_count += 1
+                    print(exception_count)
