7c22c4206398b4f09e63d495feee7ee48f814834
Soran <soran.gdr.cs@gmail.com>
2019-12-15 05:40:24 +0330

    plot_ROC function: 1. Plot AUC

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -11,3 +11,6 @@ def plot_ROC(fpr, tpr, met):
     auc = metrics.auc(fpr, tpr)
     # auc = np.trapz(tpr, fpr)
     print('AUC:', auc)
+    # Print ROC curve
+    plt.plot(fpr, tpr, label='AUC -> %s (area = %0.2f)' % (met, auc))
+    # plt.show()
