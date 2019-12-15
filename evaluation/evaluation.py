dbe528bedfdefcae79157ef91a46a60bbf72b56a
Soran <soran.gdr.cs@gmail.com>
2019-12-15 05:44:36 +0330

    _evaluate_method function: 1. Handle function to evaluate 2. Generate train graph

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -31,3 +31,23 @@ def _evaluate_method(G, k, method):
     auc_sum = 0
     print(tabulate([[f'Starting caculating {method}']], tablefmt='grid'))
     iterator = 0
+
+    for train_index, test_index in kf.split(list(G.edges)):
+        G_train = G.copy()
+        np_edges = np.array(list(G.edges))
+        test_edges = np_edges[test_index]
+        G_train.remove_edges_from(test_edges)
+        # print('G_train(node, edge): ', G_train.number_of_nodes(), G_train.number_of_edges())
+        print('Iteration %i / %i :' % (iterator, k))
+        # -------------------------------------------------------------------
+        if method == 'jc':
+            predicted = pr.jc_predict(G_train)
+        elif method == 'aa':
+            predicted = pr.aa_predict(G_train)
+        elif method == 'cn':
+            predicted = pr.cn_predict(G_train)
+        elif method == 'pa':
+            predicted = pr.pa_predict(G_train)
+        else:
+            raise Exception('Entered method is not valid', method)
+        # -------------------------------------------------------------------
