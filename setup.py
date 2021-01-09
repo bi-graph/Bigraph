49b3edd22d6dea363c1e3ef93ba072d06e80365c
Soran <soran.gdr.cs@gmail.com>
2021-01-10 03:25:22 +0330

    1. Modify package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pandas==0.24.2", "networkx==2.2"]
 
 setup(
     name="bigraph",
-    version="0.2.1",
+    version="0.2.2",
     author="Soran Ghadri",
     author_email="jeffmshale@gmail.com",
     description="Link prediction in bipartite graphs",
