4657ae32d9b8e9c206d4baf57adb58c77f6bdd9c
Soran <soran.gdr.cs@gmail.com>
2021-01-09 06:28:05 +0330

    Create setup.py module

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["networkx==2.2", "numpy==1.19.5", "matplotlib==3.3.3"]
+
+setup(
+    name="bigraph",
+    version="0.1.0",
+    author="Soran Ghadri",
+    author_email="jeffmshale@gmail.com",
+    description="Link prediction in bipartite graphs",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/soran-ghadri/bigraph",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6+",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
