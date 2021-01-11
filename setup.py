a470b4788be02c732db164ab827a8871917661ac
Soran <soran.gdr.cs@gmail.com>
2021-01-11 18:39:01 +0330

    Modify setup.py

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ requirements = parse_requirements_file("requirements/default.txt")
 
 setup(
     name=name,
-    version="0.1rc3",
+    version="0.1rc4",
     maintainer=maintainer,
     maintainer_email=maintainer_email,
     author=authors["Soran"][0],
